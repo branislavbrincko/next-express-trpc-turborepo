2bd703caca4c884117226ff226a9f889a6d19481
Branislav Brincko <branislav.brincko@goodrequest.com>
2024-03-26 17:06:39 +0100

    Basic setup expres -> vanilla client working

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,75 @@
-import { createServer } from "./server";
+/**
+ * This a minimal tRPC server
+ */
+import { initTRPC } from '@trpc/server'
+import { db } from './db'
+import { z } from 'zod'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import express from 'express'
+import cors from 'cors'
 
-const port = process.env.PORT || 3001;
-const server = createServer();
+// created for each request
+const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({}) // no context
+type Context = Awaited<ReturnType<typeof createContext>>
+/**
+ * Initialization of tRPC backend
+ * Should be done only once per backend!
+ */
+const t = initTRPC.context<Context>().create()
 
-server.listen(port, () => {
-  console.log(`api running on ${port}`);
-});
+/**
+ * Export reusable router and procedure helpers
+ * that can be used throughout the router
+ */
+export const router = t.router
+export const publicProcedure = t.procedure
+
+const appRouter = router({
+    user: {
+        list: publicProcedure.query(async () => {
+            // Retrieve users from a datasource, this is an imaginary database
+            const users = await db.user.findMany()
+            //    ^?
+            return users
+        }),
+        byId: publicProcedure.input(z.string()).query(async (opts) => {
+            const { input } = opts
+            //      ^?
+            // Retrieve the user with the given ID
+            const user = await db.user.findById(input)
+            return user
+        }),
+        create: publicProcedure.input(z.object({ name: z.string() })).mutation(async (opts) => {
+            const { input } = opts
+            //      ^?
+            // Create a new user in the database
+            const user = await db.user.create(input)
+            //    ^?
+            return user
+        }),
+    },
+})
+
+// Export type router type signature, this is used by the client.
+export type AppRouter = typeof appRouter
+
+// const server = createHTTPServer({
+//     router: appRouter,
+// })
+
+// server.listen(3001)
+
+const app = express();
+
+app.use(cors())
+
+app.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+
+
+app.listen(3001);
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,66 +1,64 @@
-"use client";
+'use client'
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
+import { trpc } from '../client'
 
-const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "http://localhost:3001";
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:3001'
 
 export default function Web() {
-  const [name, setName] = useState<string>("");
-  const [response, setResponse] = useState<{ message: string } | null>(null);
-  const [error, setError] = useState<string | undefined>();
+    const [name, setName] = useState<string>('')
+    const [response, setResponse] = useState<{ message: string } | null>(null)
+    const [error, setError] = useState<string | undefined>()
 
-  useEffect(() => {
-    setResponse(null);
-    setError(undefined);
-  }, [name]);
+    useEffect(() => {
+        setResponse(null)
+        setError(undefined)
+    }, [name])
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setName(e.target.value);
+    useEffect(() => {
+        trpc.user.list.query().then(r => console.log('r',r))
+    }, [])
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)
 
-    try {
-      const result = await fetch(`${API_HOST}/message/${name}`);
-      const response = await result.json();
-      setResponse(response);
-    } catch (err) {
-      console.error(err);
-      setError("Unable to fetch response");
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        try {
+            const result = await fetch(`${API_HOST}/message/${name}`)
+            const response = await result.json()
+            setResponse(response)
+        } catch (err) {
+            console.error(err)
+            setError('Unable to fetch response')
+        }
     }
-  };
 
-  const onReset = () => {
-    setName("");
-  };
+    const onReset = () => {
+        setName('')
+    }
 
-  return (
-    <div>
-      <h1>Web</h1>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="name">Name </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={onChange}
-        ></input>
-        <button type="submit">Submit</button>
-      </form>
-      {error && (
+    return (
         <div>
-          <h3>Error</h3>
-          <p>{error}</p>
+            <h1>Web</h1>
+            <form onSubmit={onSubmit}>
+                <label htmlFor='name'>Name </label>
+                <input type='text' name='name' id='name' value={name} onChange={onChange}></input>
+                <button type='submit'>Submit</button>
+            </form>
+            {error && (
+                <div>
+                    <h3>Error</h3>
+                    <p>{error}</p>
+                </div>
+            )}
+            {response && (
+                <div>
+                    <h3>Greeting</h3>
+                    <p>{response.message}</p>
+                    <button onClick={onReset}>Reset</button>
+                </div>
+            )}
         </div>
-      )}
-      {response && (
-        <div>
-          <h3>Greeting</h3>
-          <p>{response.message}</p>
-          <button onClick={onReset}>Reset</button>
-        </div>
-      )}
-    </div>
-  );
+    )
 }
